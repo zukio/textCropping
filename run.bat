d83112be3a489cbeb90b54812d29b591df49215f
zukio <as.web554@gmail.com>
2025-06-16 17:12:43 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -13,15 +13,13 @@ call .venv\Scripts\activate.bat
 
 REM Pythonスクリプト実行
 python main.py ^
---ignore_subfolders false ^
 --ip localhost ^
 --port 12345 ^
---send_interval 1 ^
---single_instance_only true ^
+--delay 1 ^
 --target "." ^
 --output_dir "..\output" ^
 --color_mode="mono" ^
---mono_color="#FF0000"
+--color="#FF0000"
 REM --crop オプションは--crop と書くと True になります。指定しない場合は False になります（デフォルト値）。--crop=False という構文はエラーになります。
 
 REM エラーコードを返す
